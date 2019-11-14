520cbbdea8d127b92b4cd0e608f7a565792bbaa9
aaronlyt <mlaaronlyt@gmail.com>
2019-11-14 21:53:30 +0800

    tfrecord read and write

--- utils/netflix.py
+++ utils/netflix.py
@@ -8,33 +8,22 @@ import numpy as np
 import tensorflow as tf
 
 
-def make_netflix_dataset(train_dir, dev_path, test_path, data_dir, \
-    batch_size, epochs, cache=True):
+def make_netflix_dataset(data_dir, train_lens, batch_size, epochs):
     """
     """
-    if cache and os.path.exists(os.path.join(data_dir, "datas.dump")):
-        datas = json.load(open(os.path.join(data_dir, "datas.dump"), "r"))
-        train_datas, dev_datas = datas["train"], datas["dev"]
-        movie_vocab, user_vocab = datas["movie_vocab"], datas["user_vocab"]
-    else:
-        train_datas, dev_datas, movie_vocab, user_vocab = \
-            read_traindev_data(train_dir, dev_path, data_dir, cache)
-    test_datas = read_test_data(test_path, user_vocab, movie_vocab)
+    tra_tfpath = os.path.join(data_dir, "tra.tfrecord")
+    dev_tfpath = os.path.join(data_dir, "dev.tfrecord")
+    test_tfpath = os.path.join(data_dir, "test.tfrecord")
 
-    train_dataset = tf.data.Dataset.from_tensor_slices(\
-        ({"user_id": np.array([[sample[1]] for sample in train_datas]), \
-            "item_id": np.array([[sample[0]] for sample in train_datas])}, \
-            np.array([sample[2] for sample in train_datas])))
-    dev_dataset = tf.data.Dataset.from_tensor_slices(\
-        ({"user_id": np.array([[sample[1]] for sample in dev_datas]), \
-            "item_id": np.array([[sample[0]] for sample in dev_datas])}, \
-                np.array([sample[2] for sample in dev_datas])))
-    dev_dataset = tf.data.Dataset.from_tensor_slices(\
-        ({"user_id": np.array([[sample[1]] for sample in test_datas]), \
-            "item_id": np.array([[sample[0]] for sample in test_datas])}, \
-                np.array([sample[2] for sample in test_datas])))
+    train_dataset = tf.data.TFRecordDataset(tra_tfpath)
+    dev_dataset = tf.data.TFRecordDataset(dev_tfpath)
+    test_dataset = tf.data.TFRecordDataset(test_tfpath)
 
-    train_dataset = train_dataset.shuffle(len(train_datas))
+    train_dataset = train_dataset.map(_parse_function)
+    dev_dataset = dev_dataset.map(_parse_function)
+    test_dataset = test_dataset.map(_parse_function)
+
+    train_dataset = train_dataset.shuffle(train_lens)
     train_dataset = train_dataset.repeat(epochs)
     train_dataset = train_dataset.batch(batch_size)
     train_dataset = train_dataset.prefetch(2)
@@ -45,10 +34,18 @@ def make_netflix_dataset(train_dir, dev_path, test_path, data_dir, \
     test_dataset = dev_dataset.batch(batch_size)
     test_dataset = dev_dataset.prefetch(2)
 
-    return train_dataset, dev_dataset, test_dataset, len(movie_vocab), len(user_vocab)
+    return train_dataset, dev_dataset, test_dataset
 
 
-def read_traindev_data(train_dir, dev_path, data_dir, cache=True):
+def make_tfrecords(train_dir, dev_path, test_path, data_dir):
+    """
+    """
+    tra_c, dev_c, movie_vocab, user_vocab = \
+        read_traindev_data(train_dir, dev_path, data_dir)
+    test_c = read_test_data(test_path, data_dir, movie_vocab, user_vocab)
+    return True, tra_c, dev_c, test_c
+
+def read_traindev_data(train_dir, dev_path, data_dir):
     """
     get the dev ratings from train adn remove dev from train
     """
@@ -62,52 +59,88 @@ def read_traindev_data(train_dir, dev_path, data_dir, cache=True):
                 userid = line.strip()
                 dev_datas["%s_%s" %(movie_id, userid)] = 0
     
-    train_datas = []
-    ldev_datas = []
     user_id_map = {}
     movie_id_map = {}
-    for filename in os.listdir(train_dir):
-        filepath = os.path.join(train_dir, filename)
-        with open(filepath, "r") as reader:
-            movie_id = next(reader).strip().split(":")[0]
-            movie_id_map.setdefault(movie_id, len(movie_id))
-            for user_line in reader:
-                userid, rating, timestamp = user_line.strip().split(",")
-                user_id_map.setdefault(userid, len(user_id_map))
-                sample = [movie_id_map[movie_id], user_id_map[userid], float(rating), timestamp]
-                if "%s_%s" %(movie_id, userid) not in dev_datas:
-                    train_datas.append(sample)     
-                else:
-                    ldev_datas.append(sample)
-                
-    print("---train dev dataset summary----", len(train_datas), len(ldev_datas))
+    train_tfpath = os.path.join(data_dir, "tra.tfrecord")
+    dev_tfpath = os.path.join(data_dir, "dev.tfrecord")
+    train_count = 0
+    dev_count = 0
+    with tf.io.TFRecordWriter(train_tfpath) as tra_writer, \
+        tf.io.TFRecordWriter(dev_tfpath) as dev_writer:
+        for filename in os.listdir(train_dir):
+            filepath = os.path.join(train_dir, filename)
+            with open(filepath, "r") as reader:
+                movie_id = next(reader).strip().split(":")[0]
+                movie_id_map.setdefault(movie_id, len(movie_id))
+                for user_line in reader:
+                    userid, rating, timestamp = user_line.strip().split(",")
+                    user_id_map.setdefault(userid, len(user_id_map))
+                    sample = [movie_id_map[movie_id], user_id_map[userid], float(rating), timestamp]
+                    example = serialize_example(sample[0], sample[1], sample[2])
+                    if "%s_%s" %(movie_id, userid) not in dev_datas:
+                        tra_writer.write(example)
+                        train_count += 1
+                    else:
+                        dev_writer.write(example)
+                        dev_count += 1
+    print("-----train and dev dataset length----", train_count, dev_count)
     print("---vocab summary----", len(user_id_map), len(movie_id_map))
-   
-    if cache:
-        json.dump({"train": train_datas, "dev": ldev_datas, \
-            "user_vocab": user_id_map, "movie_vocab": user_id_map}, \
-                open(os.path.join(data_dir, "datas.dump"), "w"))
+    json.dump({"user_vocab": user_id_map, "movie_vocab": user_id_map, \
+        "train_count": train_count, "dev_count": dev_count}, \
+        open(os.path.join(data_dir, "datas.dump"), "w"))
 
-    return train_datas, ldev_datas, movie_id_map, user_id_map
+    return train_count, dev_count, movie_id_map, user_id_map
 
 
-def read_test_data(test_path, movie_vocab, user_vocab):
+def read_test_data(test_path, data_dir, movie_vocab, user_vocab):
     """
     """
-    test_datas = {}
-    with open(test_path, "r") as reader:
-        movie_id = ""
-        for line in reader:
-            if ":" in line:
-                movie_id = line.strip().split(":")[0]
-            else:
-                try:
+    test_datas_count = 0
+    test_tfpath = os.path.join(data_dir, "test.tfrecord")
+    with tf.io.TFRecordWriter(test_tfpath, "w") as test_tfwriter:
+        with open(test_path, "r") as reader:
+            movie_id = ""
+            for line in reader:
+                if ":" in line:
+                    movie_id = line.strip().split(":")[0]
+                else:
                     userid, timestamp = line.strip().split(",")
-                except:
-                    print(line)
-                #test_datas.append([\
-                #    movie_vocab[movie_id], user_vocab[userid], 0, timestamp])
-    return test_datas
+                    sample = [movie_vocab[movie_id], user_vocab[userid], 0, timestamp]
+                    example = serialize_example(sample[0], sample[1], sample[2])
+                    test_tfwriter.write(example)
+                    test_datas_count += 1
+    print("----test dataset cout------", test_datas_count)
+    return test_datas_count
+
+
+def serialize_example(movieid, userid, rating):
+    """
+    """
+    feature = {
+        "movie_id": _int_feature(movieid),
+        "user_id": _int_feature(userid),
+        "rating": _float_feature(rating)
+    }
+    example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+    return example_proto.SerializeToString()
+
+
+def _parse_function(example_proto):
+    feature_description = {
+        'movie_id': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+        'user_id': tf.io.FixedLenFeature([], tf.int64, default_value=0),
+        'rating': tf.io.FixedLenFeature([], tf.float, default_value=0.0)
+        }
+    return tf.io.parse_single_example(example_proto, feature_description)
+
+
+def _float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def _int_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
 
 if __name__ == "__main__":
     train_dir = "/home/lyt/workspace/recsys/data/netflixprize/training_set"
